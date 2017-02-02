e57256b8607694e6c1d41811ad8155f907215015
Tanya Shelipova <stn.brainstorm@gmail.com>
2017-02-02 23:12:14 +0800

    Умножение issue #2

--- Main.java
+++ Main.java
@@ -13,15 +13,23 @@ class Main {
         String[] sArray2 = s.split(" ");
         System.out.println(subtraction(Integer.valueOf(sArray2[0]),Integer.valueOf(sArray2[1])));
 		
+		scan = new Scanner(System.in);
+        s = scan.nextLine();
+        String[] sArray3 = s.split(" ");
+        System.out.println(multiplication(Integer.valueOf(sArray3[0]),Integer.valueOf(sArray3[1])));
+
     }
 
     public static int addition(int a, int b) {
         int sum = a + b;
         return sum;
     }
-	
     public static int subtraction(int a, int b) {
         int sum = a - b;
+        return sum;
+    }
+    public static int multiplication(int a, int b) {
+        int sum = a * 2;
         return sum;
     }
 }
