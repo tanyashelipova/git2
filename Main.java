107a8887f5b22217c65721c1f0e978710d39c681
Tanya Shelipova <stn.brainstorm@gmail.com>
2017-02-02 23:09:58 +0800

    Вычитание issue #1

--- Main.java
+++ Main.java
@@ -8,11 +8,20 @@ class Main {
         String[] sArray = s.split(" ");
         System.out.println(addition(Integer.valueOf(sArray[0]),Integer.valueOf(sArray[1])));
 
+        scan = new Scanner(System.in);
+        s = scan.nextLine();
+        String[] sArray2 = s.split(" ");
+        System.out.println(subtraction(Integer.valueOf(sArray2[0]),Integer.valueOf(sArray2[1])));
+		
     }
 
     public static int addition(int a, int b) {
         int sum = a + b;
         return sum;
     }
-
+	
+    public static int subtraction(int a, int b) {
+        int sum = a - b;
+        return sum;
+    }
 }
